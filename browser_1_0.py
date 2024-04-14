3ffcc18f742d392dc12ed569fe1ca8fc4742c8c7
anishry <84318759+anishry@users.noreply.github.com>
2024-04-14 12:22:58 +0530

    Add files via upload

---
+++ browser_1_0.py
@@ -0,0 +1,95 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://testmcq.site/"))
+        self.browser.urlChanged.connect(self.update_urlbar)
+        self.browser.loadFinished.connect(self.update_title)
+        self.setCentralWidget(self.browser)
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+        navtb = QToolBar("Navigation")
+        self.addToolBar(navtb)
+
+        back_btn = QAction("Back", self)
+        back_btn.setStatusTip("Back to previous page")
+        back_btn.triggered.connect(self.browser.back)
+        navtb.addAction(back_btn)
+
+        next_btn = QAction("Forward", self)
+        next_btn.setStatusTip("Forward to next page")
+        next_btn.triggered.connect(self.browser.forward)
+        navtb.addAction(next_btn)
+
+        reload_btn = QAction("Reload", self)
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(self.browser.reload)
+        navtb.addAction(reload_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.setStatusTip("Go home")
+        home_btn.triggered.connect(self.navigate_home)
+        navtb.addAction(home_btn)
+
+        navtb.addSeparator()
+
+        self.urlbar = QLineEdit()
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+        navtb.addWidget(self.urlbar)
+
+        stop_btn = QAction("Stop", self)
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(self.browser.stop)
+        navtb.addAction(stop_btn)
+
+        self.menuBar().setHidden(True)  # Hide the menu bar
+        navtb.setHidden(True)  # Hide the navigation toolbar
+        self.urlbar.setHidden(True)  # Hide the URL bar
+
+        self.showFullScreen()  # Launch in full-screen mode
+
+    def update_title(self):
+        title = self.browser.page().title()
+        self.setWindowTitle("%s - Online Exam" % title)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("http://testmcq.site/"))
+
+    def navigate_to_url(self):
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+        self.browser.setUrl(q)
+
+    def update_urlbar(self, q):
+        self.urlbar.setText(q.toString())
+        self.urlbar.setCursorPosition(0)
+        
+    #def contextMenuEvent(self, event):
+        # Override context menu event to disable right-clicking
+        #event.ignore()
+        
+
+app = QApplication(sys.argv)
+app.setApplicationName("Online Exam")
+
+#app_icon = QIcon("icon.png")
+#app.setWindowIcon(app_icon)
+
+window = MainWindow()
+
+# Install event filter to handle file open events
+#app.installEventFilter(window)
+
+
+app.exec_()
